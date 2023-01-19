95e4b6fc7749a9cabf9960a5a0c407e5582ba9e9
bablu <bk147411@gmail.com>
2023-01-20 00:23:31 +0530

    update code

--- src/PagesComponents/CpdAndConferences/AddNewCpd.tsx
+++ src/PagesComponents/CpdAndConferences/AddNewCpd.tsx
@@ -13,7 +13,7 @@ import {
 } from "@mui/material";
 import styles from "@/styles/Home.module.css";
 
-const CpdAndConferencesComponent = () => {
+const AddNewCpdComponent = ({setShowAddNew}) => {
   const [type, setType] = useState("Any");
 
   const handleChange = (event: SelectChangeEvent) => {
@@ -22,7 +22,7 @@ const CpdAndConferencesComponent = () => {
 
   const typeOfEvent = [
     {
-      label: "Any",
+      label: "BANT Accredited Professional Supervision",
       value: "Any",
     },
     {
@@ -36,65 +36,54 @@ const CpdAndConferencesComponent = () => {
   ];
 
   return (
-      <Box component="main" className={styles.main} id="wrapper">
-        <Box>
-          <h2 className="">Type of Event / Activity</h2>
-        
-        </Box>
-       
-        <Box className="cpdform_main">
-         
-
-          <Box className="left-side-select">
-            <Box className="form_Group">
-              <InputLabel>What kind of event / activity is it?</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                label="Age"
-                onChange={handleChange}
-                value={type}
-              >
-                <MenuItem value={""}>Any</MenuItem>
-                {typeOfEvent?.map((val, index) => {
-                  return (
-                    <MenuItem key={index} value={val?.value}>
-                      {val?.label}
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-            </Box>
-          </Box>
-
-          <Box className="right_side-select">
-            <Box className="form_Group">
-              <InputLabel>Event / Activity Title</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                label="Age"
-                onChange={handleChange}
-                value={type}
-              >
-                <MenuItem value={""}>Any</MenuItem>
-                {typeOfEvent?.map((val, index) => {
-                  return (
-                    <MenuItem key={index} value={val?.value}>
-                      {val?.label}
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-            </Box>
-          </Box>
-
-         
-        </Box>
-
-       
+    <Box component="main" className={styles.main} id="wrapper">
+      <Box>
+        <h2 className="">Type of Event / Activity</h2>
       </Box>
+
+      <Box className="cpdform_main">
+        <Box className="left-side-select">
+          <Box className="form_Group">
+            <InputLabel>What kind of event / activity is it?</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              label="Age"
+              onChange={handleChange}
+              value={type}
+            >
+              <MenuItem value={""}>Any</MenuItem>
+              {typeOfEvent?.map((val, index) => {
+                return (
+                  <MenuItem key={index} value={val?.value}>
+                    {val?.label}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </Box>
+        </Box>
+
+        <Box className="right_side-select">
+          <Box className="form_Group">
+            <InputLabel>Event / Activity Title</InputLabel>
+            <Input
+              type="text"
+              placeholder="BANT Accredited Professional Supervision Group"
+              value={"BANT Accredited Professional Supervision Group"}
+            />
+          </Box>
+        </Box>
+      </Box>
+
+      <Box className="Button for add_section">
+        <Button className="back" onClick={()=>setShowAddNew()}>{'<'} Back</Button>
+        <Button className="next">Next {'>'} </Button>
+        <Button className="cancel" onClick={()=>setShowAddNew()} >Cancel</Button>
+
+      </Box>
+    </Box>
   );
 };
 
-export default CpdAndConferencesComponent;
+export default AddNewCpdComponent;
